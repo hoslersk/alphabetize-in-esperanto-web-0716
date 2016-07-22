7a19be50f607c67b47389c0bc1f44a3278de1504
Schyler Hosler <schylerhosler@gmail.com>
2016-07-22 02:16:26 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,20 @@
 def alphabetize(arr)
   # code here
+  #sorted_arr = []
+  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  #arr.each do |word|
+  #  esperanto_arr.each do |letter|
+  #    if word[0] == letter
+  #      sorted_arr << word
+  #    end
+  #  end
+  #end
+  arr.sort_by do |phrase|
+    phrase.chars.map do |character|
+      esperanto_arr.index(character)
+    end
+  end
+  #return sorted_arr
 end
+
+#result = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
