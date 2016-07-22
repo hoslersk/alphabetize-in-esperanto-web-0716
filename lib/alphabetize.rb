713d8175680de762307c61581ef68bdd4bf26a9e
Schyler Hosler <schylerhosler@gmail.com>
2016-07-22 02:29:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,14 @@
 def alphabetize(arr)
   # code here
   #sorted_arr = []
-  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars # or...
+  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars # same as...
+  #esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.to_a # or...
+
   #esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.with_index.to_h
   arr.sort_by do |phrase|
     phrase.chars.map do |character|
       esperanto_arr.index(character) # or...
+
       #esperanto_arr[c]
     end
   end
